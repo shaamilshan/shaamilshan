21437980b1a88cfc892122e2a25f2a96c230152c
shaamilshan <shmlvk123@gmail.com>
2025-08-22 14:52:15 +0530

    devagiri cc

--- fruitskada.py
+++ fruitskada.py
@@ -1,11 +1,19 @@
 stock =  [] 
 
-
-
 n = int(input("Enter no of products to add: "))
 for i in range(n):
     product = input("Enter product name")
     price = int(input("Enter product price"))
     quantity = int(input("Enter product quantity"))
 
-    if price > 50: || quantity > 10
+    if price > 50 or quantity > 10:
+        discount = price * 0.5
+        price -= discount
+        print(f"Discount applied. New price: {price:.2f}")
+    else:
+        print("No discount for you")
+    stock.append({"Product": product, "Price": round(price, 2), "Quantity": quantity})
+
+print("Final Stock:")
+for item in stock:
+    print(item)
