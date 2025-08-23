6ecc30d5d3de66481cfb91c85c4f32a3e97581e3
shaamilshan <shmlvk123@gmail.com>
2025-08-23 13:25:16 +0530

    next

--- pandalearn.py
+++ pandalearn.py
@@ -2,6 +2,17 @@ import pandas as pd
 s = pd.Series([10,20,30,40])
 print(s)
 
-data = {'Name' : ['Shan','Siuu Annan'], 'Age' : [21,40]}
+data = {'Name' : ['Shan','Siuu Annan','shanumon','shaamilshan','shottappan','Obama','Kudu'], 'Age' : [21,40,22,33,21,34,26]}
 df = pd.DataFrame(data)
 print(df)
+print(df.head())
+print(df.head(3))
+print(df.tail())
+print(df.tail(3))
+print(df.info())
+print(df.describe())
+print(df.columns)
+print(df.shape)
+
+print(df.iloc[0])
+print(df.iloc[:,0:1])
